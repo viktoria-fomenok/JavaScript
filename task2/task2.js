d31cd8fb5de848f0ea1f33bf72cb28b5e1ec8ac5
Viktoria Fomenok <viky.fomenok@gmail.com>
2017-04-03 09:56:31 +0300

    fix_task2

--- task2/task2.js
+++ task2/task2.js
@@ -2,18 +2,25 @@
  * Created by vikyf on 3/31/2017.
  */
 
-
 //////////////Task1
 function findMin3() {
     var min3 = [];
     min3 = prompt("Input value of the array with a space: ", "");
     min3 = min3.split(' ');
+
+    for (var i = min3.length - 1; i > 0; i--) {
+        if (min3[i] == min3[i - 1]) {
+            min3.splice( i, 1);
+        }
+    }
+
     if(min3.length < 3){
         alert("Array must be more than 2 value")
     }
     else{
         min3.sort();
-        alert("3 minimum value of the array = " + min3[2]);
+        alert("Sort array = " + min3
+            + "\n3minimum value of the array = " + min3[2]);
     }
 }
 
